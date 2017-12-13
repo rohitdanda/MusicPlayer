d573428c37e547aede897036ae7a688c44d6f8b8
rohitdanda <rohitdanda@gmail.com>
2017-12-13 01:46:49 +0000

    New Fixes permission

---
+++ app/src/main/java/com/example/sairohit/musicplayerinterface/MusicService.java
@@ -0,0 +1,125 @@
+package com.example.sairohit.musicplayerinterface;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import java.util.ArrayList;
+import android.content.ContentUris;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.PowerManager;
+import android.util.Log;
+import android.widget.MediaController;
+
+/**
+ * Created by sairohit on 13/12/17.
+ */
+
+public class MusicService extends Service implements MediaPlayer.OnPreparedListener,MediaPlayer.OnErrorListener,MediaPlayer.OnCompletionListener{
+    @Nullable
+
+    private MediaPlayer player;
+
+    MediaController mediaController;
+    //song list
+    private ArrayList<SongsDetail> songs;
+    //current position
+    private int songPosn;
+    private final IBinder musicBind = new MusicBinder();
+
+    public void setSongs(ArrayList<SongsDetail> songs) {
+        this.songs = songs;
+    }
+
+    public class MusicBinder extends Binder {
+        MusicService getService() {
+            return MusicService.this;
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        songPosn=0;
+        player = new MediaPlayer();
+
+        initMusicPlayer();
+
+
+
+    }
+    public void initMusicPlayer(){
+        //set player properties
+        player.setWakeMode(getApplicationContext(),
+                PowerManager.PARTIAL_WAKE_LOCK);
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        player.setOnPreparedListener(this);
+        player.setOnCompletionListener(this);
+        player.setOnErrorListener(this);
+
+    }
+
+
+    public IBinder onBind(Intent intent) {
+        return musicBind;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent){
+        player.stop();
+        player.release();
+        return false;
+    }
+
+
+    @Override
+    public void onCompletion(MediaPlayer mediaPlayer) {
+
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mediaPlayer, int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
+
+        mediaPlayer.start();
+
+    }
+
+    public void playSong(){
+        //play a song
+
+        player.reset();
+
+        //get song
+        SongsDetail playSong = songs.get(songPosn);
+//get id
+        long currSong = playSong.getId();
+//set uri
+        Uri trackUri = ContentUris.withAppendedId(
+                android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                currSong);
+
+        try{
+            player.setDataSource(getApplicationContext(), trackUri);
+        }
+        catch(Exception e){
+            Log.e("MUSIC SERVICE", "Error setting data source", e);
+        }
+
+        player.prepareAsync();
+    }
+
+    public void setSong(int songIndex){
+        songPosn=songIndex;
+    }
+
+}
